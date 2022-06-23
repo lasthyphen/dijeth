029229b6b1a71d129deade3107a7056c8139dc0f
Dijets <39377778+lasthyphen@users.noreply.github.com>
2022-06-23 15:31:19 +0100

    Update config.go

--- params/config.go
+++ params/config.go
@@ -38,11 +38,11 @@ import (
 // Avalanche ChainIDs
 var (
 	// AvalancheMainnetChainID ...
-	AvalancheMainnetChainID = big.NewInt(43114)
+	AvalancheMainnetChainID = big.NewInt(98200)
 	// AvalancheFujiChainID ...
-	AvalancheFujiChainID = big.NewInt(43113)
+	AvalancheFujiChainID = big.NewInt(98199)
 	// AvalancheLocalChainID ...
-	AvalancheLocalChainID = big.NewInt(43112)
+	AvalancheLocalChainID = big.NewInt(98198)
 
 	errNonGenesisForkByHeight = errors.New("coreth only supports forking by height at the genesis block")
 )
